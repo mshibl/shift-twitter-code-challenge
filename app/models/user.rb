3215f328c3a6d2db0ee91f5063c4531757c0fa7e
mshibl <mohamed.shibl@hotmail.com>
2016-02-02 21:43:44 -0800

    Adds tweet model and migration, and adds association in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :followers, class_name: "Relationship", foreign_key: "friend_id"
 	has_many :friends, class_name: "Relationship", foreign_key: "follower_id"
+	has_many :tweets
 end
