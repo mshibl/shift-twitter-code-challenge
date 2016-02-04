6f3fc8f9e8b3989f10ceb426921ab3fa9cebe506
mshibl <mohamed.shibl@hotmail.com>
2016-02-04 12:25:37 -0800

    adds accounts for Meg and Eugene in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+@meg = User.new(first_name: 'Meg', last_name: 'Nakamura', email: 'meg@shiftpayments.com')
+@meg.password = 123456
+@meg.save!
+
+@eugene = User.new(first_name: 'Eugene', last_name: 'Otto', email: 'eugene@shiftpayments.com')
+@eugene.password = 123456
+@eugene.save!
+
 50.times do
   @user = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name ,  email: Faker::Internet.email)
   @user.password = 123456
@@ -5,9 +13,9 @@
 end
 
 300.times do
-	Tweet.create!(user_id: rand(1..57), text: Faker::Lorem.sentence(3))
+	Tweet.create!(user_id: rand(1..52), text: Faker::Lorem.sentence(3))
 end
 
 500.times do
-	Relationship.create!(friend_id: rand(1..57), follower_id: rand(1..57))
+	Relationship.create!(friend_id: rand(1..52), follower_id: rand(1..57))
 end
