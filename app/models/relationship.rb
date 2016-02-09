94c39cfc7b9351a085a37fa2517fbfb2b1bc3c5c
mshibl <mohamed.shibl@hotmail.com>
2016-02-08 21:22:14 -0800

    adds changes to relationship.rb

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,8 @@ class Relationship < ActiveRecord::Base
 	belongs_to :friend, class_name: 'User'
 	belongs_to :follower, class_name: 'User'
 	validates :friend_id, :follower_id, presence: true
+
+	def cannot_follow_self
+  	
+  	end
 end
